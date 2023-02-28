be9c0212424c14e7bcfd59bbff9bf8995c617062
ckswo00 <ckswo00@naver.com>
2023-03-01 00:23:09 +0900

    1.0.1

--- src/chap_02/_04_Operator4.java
+++ src/chap_02/_04_Operator4.java
@@ -24,5 +24,6 @@ public class _04_Operator4 {
         System.out.println(!false); // true
         System.out.println(!(5 == 5)); // false
         System.out.println(!(5 == 3)); // true
+
     }
 }
